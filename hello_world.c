b0f80a1bf62f28f0917ecb98fa2ff5a21b38200f
Hirohito Higashi <higashi@s-itoc.jp>
2016-06-07 12:40:35 +0900

    on master branch.

--- hello_world.c
+++ hello_world.c
@@ -5,5 +5,6 @@ int main( int argc, char *argv[] )
 {
   printf( "Hello another world in feature-one.\n" );
   printf( "Add by subversion.\n" );
+  printf( "on Master branch.\n" );
   return EXIT_SUCCESS;
 }
