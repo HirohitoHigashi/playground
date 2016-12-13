0bc7dc1e1fda639b809275f9a32e669b578bec02
Hirohito Higashi <uzuki.local>
2016-12-13 15:18:10 +0900

    bugfix.

--- hello_world.c
+++ hello_world.c
@@ -6,7 +6,7 @@ int main( int argc, char *argv[] )
   printf( "Hello another world in feature-one.\n" );
   printf( "on Master branch.\n" );
 
-  printf( "branch-push-merge\n" );
+  printf( "branch-push-merge.\n" );
 
   return EXIT_SUCCESS;
 }
